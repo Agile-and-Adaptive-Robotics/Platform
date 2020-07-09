59ee2ca7d722a89a2dded696acd88fcfc2a3be09
Jake Chung <luonchung@gmail.com>
2020-07-08 17:58:42 -0700

    done with this

--- Arduino Files/SineGenerationTrial/testSineWaveGen/testSineWaveGen.ino
+++ Arduino Files/SineGenerationTrial/testSineWaveGen/testSineWaveGen.ino
@@ -2,24 +2,23 @@
 //Author: Jake Chung
 //This program is developed based on Dr.Hunt algorithm
 
-#define SAMP_SIZE 100 //define the sample size of the sine value array
+#define SAMP_SIZE 50 //define the sample size of the sine value array
 
 //Define the variables
-int Amp = 128; //amplitude of the sine wave
-byte VerShift = 128; //vertical shift of the sine wave
-int pin = 3;
+int Amp = 800; //amplitude of the sine wave
+float VerShift = 2047; //vertical shift of the sine wave
 int Freq = 1; //Frequency of the sine wave (not angular frequency)
 int i = 0; //counter
-byte SetPoint[SAMP_SIZE]; //the sine value array
+int SetPoint[SAMP_SIZE]; //the sine value array
 
 void setup() {
   Serial.begin(9600);
+  analogWriteResolution(12);
   //predefine the sine value array
   for (i=1; i<SAMP_SIZE; ++i){
     SetPoint[i] = Amp*sin(Freq*6.2832/SAMP_SIZE*i)+VerShift;
   }
-  SetPoint[0] = 128; //define the first value of the sine wave
-  pinMode(pin, OUTPUT);
+  SetPoint[0] = 2047; //define the first value of the sine wave
 }
 
 void loop() {
@@ -27,6 +26,7 @@ void loop() {
     i = 0;
   }
   Serial.println(SetPoint[i]);
-  analogWrite(pin, SetPoint[i]);
+  analogWrite(DAC0, SetPoint[i]);
   ++i;
+  delay(10);
 }
