5715191a77f71f07fbe9f617ec50f522a4c23c14
Jake Chung <luonchung@gmail.com>
2020-07-22 22:37:00 -0700

    Update sCollectSineData.m

--- Matlab Files/sCollectSineData.m
+++ Matlab Files/sCollectSineData.m
@@ -5,7 +5,9 @@ function data = sCollectSineData(s)
 %open to collect. If program is terminated prematurely, do fclose
 fopen(s);
 %make the cell to collect the data
-length_to_collect = 2500;%size of data array to collect
+%this number need to match with the data size of the Arduino
+length_to_collect = 5000;
+
 %cell to store the data
 collect = cell(length_to_collect, 1);
 %remove the first 20 lines of the serial port 
@@ -19,11 +21,13 @@ for i = 1:length_to_collect
     collect{i} = fgetl(s);
 end
 disp('Collection is completed, begin converting.')
-%convert data to num array
-data = zeros(length_to_collect,2);
-for i = 1:length_to_collect
-    data(i, :) = str2num(collect{i});
-end
 %close the port
 fclose(s);
+data = collect;
+% %convert data to num array
+% data = zeros(length_to_collect,2);
+% for i = 1:length_to_collect
+%     data(i, :) = str2num(collect{i});
+% end
+
 end
