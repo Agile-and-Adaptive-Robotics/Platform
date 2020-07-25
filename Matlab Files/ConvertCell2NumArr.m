d101766dc85b46c5770f08a6cd05e64dc5c44c37
Jake Chung <luonchung@gmail.com>
2020-07-25 11:53:45 -0700

    remove print out to terminal

--- Matlab Files/ConvertCell2NumArr.m
+++ Matlab Files/ConvertCell2NumArr.m
@@ -1,10 +1,9 @@
 function out = ConvertCell2NumArr(inputCell)
 %this function converts the output cell array of the sCollectSineData
 %function to a usable matrix form
-lengthCell = length(inputCell)
+lengthCell = length(inputCell);
 out = zeros(lengthCell,2);
 for i = 1:lengthCell
     out(i,:) = str2num(inputCell{i});
-    disp(i)
 end
 end
