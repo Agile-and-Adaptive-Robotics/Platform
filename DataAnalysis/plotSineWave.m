1681d127230a1f02fecda0a47115ab36dea43c61
Jake Chung <luonchung@gmail.com>
2020-08-25 15:19:12 -0700

    plot the new data collected last week

--- DataAnalysis/plotSineWave.m
+++ DataAnalysis/plotSineWave.m
@@ -7,20 +7,26 @@ close all
 clear
 
 %load in the data
-dir1 = 'rawData\BodeRawData2.mat';
-data1 = load(dir1, 'data');
-data1 = data1.data; %convert to double type
+dir1 = 'rawData\BodeData3.mat';
+data1 = load(dir1, 'numData');
+data1 = data1.numData; %convert to double type
 
-dir2 = 'rawData\BodeRawData_motorNotRunning.mat';
-data2 = load(dir2, 'data');
-data2 = data2.data; %convert to double type
+% dir2 = 'rawData\BodeRawData_motorNotRunning.mat';
+% data2 = load(dir2, 'data');
+% data2 = data2.data; %convert to double type
+
+%scale encoder data 
+encAngle = 360/8192 * data1(:,1);
+encBit = 4096/10*encAngle;
 
 %plot the data
 index = linspace(1,100,5000);
 figure(1)
-plot(index, data1(:,2))
-hold on
-plot(index, data2(:,2))
+plot(index, data1(:,2)-2048)
+%plot(index, data2(:,2))
 ylabel('Position in Binary')
 xlabel('Time (s)')
-
+hold on
+plot(index, encBit)
+ylabel('Angular position (Encoder pulse)')
+xlabel('Time (s)')
