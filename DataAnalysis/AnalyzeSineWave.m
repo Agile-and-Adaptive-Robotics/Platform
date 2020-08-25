424b496f5ab8085ef215fcbb40c7b27ec0ed5cb6
Jake Chung <luonchung@gmail.com>
2020-08-25 15:41:21 -0700

    this frequency function doesn't really work

--- DataAnalysis/AnalyzeSineWave.m
+++ DataAnalysis/AnalyzeSineWave.m
@@ -25,14 +25,6 @@ ylabel('Angular position (pulses)')
 xlabel('Time (s)')
 title('Response position collected from the encoder')
 
+%% 
 
-%% function to calculate the frequency of the sinewave data
-function freq = detectFreq(data)
-[~, idx] = max(data(:,2))
-freqArr = zeros(1,length(idx)-1)
-for i=1:length(idx)-1
-    T = max(i+1,1)-max(i,1);
-    freqArr(i) = 1/T;
-end
-freq = mean(freqArr);
-end
+
