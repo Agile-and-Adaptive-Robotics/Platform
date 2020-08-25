9589fa65a6094c39cad7c919ad29a9ffd30e927f
Jake Chung <luonchung@gmail.com>
2020-08-25 16:10:27 -0700

    ignore temp file

--- DataAnalysis/AnalyzeSineWave.m
+++ DataAnalysis/AnalyzeSineWave.m
@@ -18,13 +18,22 @@ figure
 plot(time, raw(:,1)) %plot the input position
 ylabel('Angular position (bit)')
 xlabel('Time (s)')
-title('Desired position sent to the Platform')
+title('Response position collected from the encoder')
 figure
 plot(time, raw(:,2)) %plot the respose from the encoder
 ylabel('Angular position (pulses)')
 xlabel('Time (s)')
-title('Response position collected from the encoder')
+title('Desired position sent to the Platform')
 
-%% 
+%% detect -99 and break the raw data up to individual discrete frequency
 
+index99 = find(raw(:,1) == -99); %index of -99 values
+index99 = [1;index99]; %add the index 1 for the loop
+%split the raw data to a 3D array [time, position, frequency]
+%create holder 3D array
+encoderData = ones(length(index99),502); %502 is the maximum number of elements in each frequency
+for i = 1:length(index99)-1
+    i
+    encoderData(i,:) = raw(index99(i):index99(i+1),1)'; %seperate the frequency array based on the -99 index
+end
 
