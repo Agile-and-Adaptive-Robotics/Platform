76dca1290d704283b2718802de4ac527aa0b095f
Jake Chung <luonchung@gmail.com>
2020-09-02 13:57:43 -0700

    add frequency calculation for each output sine wave

--- DataAnalysis/AnalyzeSineWave.m
+++ DataAnalysis/AnalyzeSineWave.m
@@ -11,6 +11,7 @@ clear
 loadRaw = load('rawData/BodeData3.mat');
 raw = loadRaw.numData; %convert to double type
 sampPeriod = 0.02;
+sampFreq = 1/sampPeriod;
 
 %% plot the raw data
 %create the time index array
@@ -49,7 +50,32 @@ for i = 1:length(encoderData(:,1))
     subplot(5,2,i)
     plot(index, encoderData(i,:))
     axis([0,10,-50,50])
-    if (i == 9) || (i == 10)
+    if (i == 9) || (i == 10) %add axis in the bottom plots
         xlabel('Time (s)')
     end
+    tempTitle = num2str(calcFreq(encoderData(i,10:490)));
+    title(['Frequency ', tempTitle])
 end
+
+
+
+%% funcion to calculate the dominant frequency of a signal 
+%the frequency of a signal should be determined in the Arduino code, but
+%this function can be used to validate the frequency collected from the
+%output data.
+%original code:
+%https://www.mathworks.com/matlabcentral/answers/160059-finding-the-frequency-value-of-a-signal
+function [mainFreq] = calcFreq(data)
+%properties of the data input
+Tsamp = 0.02;
+Fsamp = 1/Tsamp;
+%choose fft size and calculate the spectrum
+Nfft = 300;
+[Pxx, f] = pwelch(data, gausswin(Nfft), Nfft/2, Nfft, Fsamp);
+[~,maxIndex] = max(Pxx);
+mainFreq = f(maxIndex);
+% figure
+% plot(f,Pxx)
+end
+
+
